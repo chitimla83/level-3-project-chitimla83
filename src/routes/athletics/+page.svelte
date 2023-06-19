8d9ffcccf8e4a27d0864d0bbed54cdf6bbd2e1f3
chitimla83 <79882087+chitimla83@users.noreply.github.com>
2023-06-20 10:23:11 +1200

    Netball Page
    Tested that the information and components are displayed correctly. tested that the column worked the way that it was expected. And that the lists are displayed how they are expected to.

--- src/routes/athletics/+page.svelte
+++ src/routes/athletics/+page.svelte
@@ -10,34 +10,97 @@
 <main>
   <h1>Athletics</h1>
   <br />
-  <h2>​LEVELS</h2>
-  <p>Years 7-13</p>
+  <div class="columns">
+    <!-- put tge images in here
+     -->
+    <div class="column" />
+    <!-- start of second column -->
+    <div class="column">
+      <h2>​LEVELS</h2>
+      <p>Years 7-13</p>
+      <br />
+      <h2>TERMS</h2>
+      <p>Terms 1 and 4</p>
+      <br />
+      <h2>IMPORTANT DATES 2023</h2>
+      <ul>Aoraki Athletics - Wednesday 16 March, Ashburton</ul>
+      <ul>South Island Athletics - 1-3 April, Christchurch</ul>
+      <ul>National Athletics - December</ul>
+      <br />
+      <h2>UNIFORM</h2>
+      <ul>PE or House coloured t-shirt/green shorts - Sports Days</ul>
+      <ul>CDS Athletics Singlet/Shorts, CDS Rugby Jersey/Merino/Jacket/House Top</ul>
+      <br />
+      <h2>Teacher in Charge: Mr Andrew Jones - jonesan@craighead.school.nz</h2>
+      <br />
+      <p>Craighead has a proud history of success and participation in athletics and developed a strong sense of 'team' in what is often perceived as an individual sport. Craighead is committed to facilitating opportunities for students to compete at a regional, South Island and national level in both athletics and cross country on an annual basis.</p>
+      <br />
+      <p>Craighead is focused on developing talent and participation working closely with the South Canterbury Amateur Athletics Club to provide coaching opportunities for those that wish to understand more about the different disciplines. Trainings are on Mondays from 4pm at the Stadium and begin in early September.</p>
+    </div>
+  </div>
+
   <br />
-  <h2>TERMS</h2>
-  <p>Terms 1 and 4</p>
+  <h2>​​​​​​​2023 School Athletics</h2>
+  <h3>Champion House Cup:</h3>
+  <li>1st TeTongai - 903 points, 2nd Te Rawhiti - 813 points, 3rd Te Uru 807 points, 4th Te Raki 787 points</li>
+  <li>Boarders vs Daygirls Relay - 1st Daygirls 2nd Boarders</li>
+  <li>Staff vs Year 13 Relay - 1st Staff 2nd Year 13</li>
+  <li>House Chant - 1st Te Uru 2nd Te Rawhiti 3rd Te Tonga</li>
+  <li>Miss J A Hayes Cup Open 100m - Sienna Wright 2nd Madeline Thomas 3rd Ruby Mahuika</li>
+  <li>Welford Family Cup for Overall Champion - Charlotte Blake/Orla Motley</li>
+  <li>Johanna Williamson (Reith) Cup for Senior Field Events - Camryn Porter</li>
+  <li>Senior Field Events Runner Up - Samantha Giddings</li>
+  <li>Open 3000m - 1st Niamh Motley 2nd Ruby Laming 3rd Faith Coffin</li>
+  <li>Kirk Cup Overall Minor Champion - Neve Ball</li>
+  <li>Minor Champion Runner Ups - Lucy Flett, Olivia McLachlan, Charlie Nichol</li>
+  <li>Mr & Mrs J Quaife Cup Overall Junior Champion - Charlotte Blake</li>
+  <li>Junior Champion Runner Up - Sienna Wright</li>
+  <li>Mrs Gilbert Grigg Challenge Cup Intermediate Overall Champion - Orla Motley</li>
+  <li>Intermediate Champion Runner Up - Maggie Robinson</li>
+  <li>Dr & Mrs Shackleton Challenge Cup Overall Senior Champion - Madeline Thomas and Niamh Motley</li>
+
   <br />
-  <h2>IMPORTANT DATES 2023</h2>
-  <ul>Aoraki Athletics - Wednesday 16 March, Ashburton</ul>
-  <ul>South Island Athletics - 1-3 April, Christchurch</ul>
-  <ul>National Athletics - December</ul>
+  <h2>CDS Athletes Enjoy Success at Aoraki Athletics</h2>
+  <p>The Craighead Athletics team enjoyed a fantastic day at the Aoraki Secondary School Track and Field championships on Wednesday 15th March in Ashburton. Held on the grass track in Ashburton Domain with the Morgans Road track currently being resurfaced, the team of 32 athletes won 17 individual Aoraki titles between them, as well as winning 3 of the 4 relays. There were many other individual place getters in a dominant display by the school and a team of over a dozen girls will now travel to the South Island Secondary Schools Track and Field Championships in Invercargill from the 31st March to 2nd April. Top results are listed below.</p>
   <br />
-  <h2>UNIFORM</h2>
-  <ul>PE or House coloured t-shirt/green shorts - Sports Days</ul>
-  <ul>CDS Athletics Singlet/Shorts, CDS Rugby Jersey/Merino/Jacket/House Top</ul>
+  <h3>Multiple title winners</h3>
+  <li>G14 – Ruby Mahuika 1st 200m, 400m and Long Jump</li>
+  <li>G15 – Sienna Wright – 100m, 200m, Long Jump</li>
+  <li>G15 – Charlotte Blake – Shot put, Discus, Javelins</li>
+  <li>G19 – Niamh Motley – 400m, 800m, 1500m.</li>
   <br />
-  <h2>Teacher in Charge: Mr Andrew Jones - jonesan@craighead.school.nz</h2>
+  <h3>Single title winners</h3>
+  <li>G14 – Charlotte Shefferd Shot Put</li>
+  <li>G14 – Ruby Laming 3000m</li>
+  <li>G15 – Faith Coffin 400m</li>
+  <li>G15 – Camryn Linton Triple Jump</li>
+  <li>G19 – Camryn Porter High Jump, also 2nd in Triple Jump</li>
   <br />
-  <p>Craighead has a proud history of success and participation in athletics and developed a strong sense of 'team' in what is often perceived as an individual sport. Craighead is committed to facilitating opportunities for students to compete at a regional, South Island and national level in both athletics and cross country on an annual basis.</p>
+  <h3>Top 3 placings</h3>
+  <li>G14 – Ruby Laming, 2nd 1500m</li>
+  <li>G15 – Zara Brunton, 2nd Discus</li>
+  <li>G15 – Faith Coffin, 2nd in 800m</li>
+  <li>G15 – Lucia O’Sullivan, 3rd Triple Jump</li>
+  <li>G16 – Mia Donaldson, 3rd 400m</li>
+  <li>G16 – Orla Motley, 2nd Triple Jump and 2nd Javelin</li>
+  <li>G16 – Tara O’Brien, 3rd High Jump</li>
+  <li>G16 – Maggie Robinson, 2nd Shot Put</li>
+  <li>G16 – Renette Van Heerden, 3rd Shot Put</li>
+  <li>G19 – Lydia Giles, 3rd 400m</li>
+  <li>G19 – Pippa Raymond, 2nd Shot put</li>
+  <li>G19 – Marissa Shannon, 2nd 800m</li>
+  <li>G19 – Madi Thomas, 2nd 200m and 400m</li>
   <br />
-  <p>Craighead is focused on developing talent and participation working closely with the South Canterbury Amateur Athletics Club to provide coaching opportunities for those that wish to understand more about the different disciplines. Trainings are on Mondays from 4pm at the Stadium and begin in early September.</p>
+  <p>Relays – 1st in G19, G16 and G15 and 3rd in G14 4 x 100m</p>
 </main>
 <Footer />
+ru
 
 <style>
   h1 {
     font-family: "Poppins";
     font-size: 40px;
-    text-align: left;
+    text-align: center;
     color: #236093;
     font-weight: bolder;
   }
@@ -50,9 +113,13 @@
     font-size: 17px;
     text-align: left;
   }
-  ul {
+  li {
     font-family: "Quattrocento Sans";
     font-size: 17px;
     text-align: left;
+  }
+  h3 {
+    font-family: "Poppins";
+    font-size: 20px;
   }
 </style>
--- src/routes/badminton/+page.svelte
+++ src/routes/badminton/+page.svelte
@@ -7,5 +7,31 @@
 
 <Nav />
 <Header title="Craighead Diocesan School" subtitle="Sports Portal" />
-
+<main>
+  <h1>Bandminton</h1>
+</main>
 <Footer />
+
+<style>
+  h1 {
+    font-family: "Poppins";
+    font-size: 40px;
+    text-align: center;
+    color: #236093;
+    font-weight: bolder;
+  }
+  h2 {
+    font-family: "Poppins";
+    font-size: 25px;
+  }
+  p {
+    font-family: "Quattrocento Sans";
+    font-size: 17px;
+    text-align: left;
+  }
+  ul {
+    font-family: "Quattrocento Sans";
+    font-size: 17px;
+    text-align: left;
+  }
+</style>
